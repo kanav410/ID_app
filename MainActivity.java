3501d67b5e306a452dbaf1260c05f7892e89929e
kanav410 <92173353+kanav410@users.noreply.github.com>
2023-01-02 09:50:02 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,27 +1,91 @@
 package com.example.finalidcardapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView dp;
+    private final int CAMERA_REQ_CODE=100;
+    //////////
+    /*
     //for name
     EditText name;
     public static final String EXTRA_NAME="com.example.finalidcardapp.extra.NAME";
     //for registration number
     EditText reg;
     public static final String EXTRA_REG="com.example.finalidcardapp.extra.REG";
+     */
+    Button submitbtn;
+    EditText nameinp,reginp,collegenameinp,collegecityinp,addressinp,bloodgrpinp,mobinp,branchinp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dp=findViewById(R.id.mypic);
+        Button btn=findViewById(R.id.camerabtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent icamera =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(icamera,CAMERA_REQ_CODE);
+
+            }
+        });
+        ///////
+
+        nameinp=findViewById(R.id.name);
+        reginp=findViewById(R.id.reg);
+        collegenameinp=findViewById(R.id.college);
+        collegecityinp=findViewById(R.id.collegecity);
+        addressinp=findViewById(R.id.address);
+        bloodgrpinp=findViewById(R.id.bloodgrp);
+        mobinp=findViewById(R.id.mob);
+        branchinp=findViewById(R.id.branch);
+
+
+
+
+        submitbtn=findViewById(R.id.submitbtn);
+        submitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name=nameinp.getText().toString();
+                String reg=reginp.getText().toString();
+                String collegename=collegenameinp.getText().toString();
+                String collegecity=collegecityinp.getText().toString();
+                String address=addressinp.getText().toString();
+                String bloodgrp=bloodgrpinp.getText().toString();
+                String mob=mobinp.getText().toString();
+                String branch=branchinp.getText().toString();
+
+                Intent intent=new Intent(MainActivity.this,printid.class);
+                intent.putExtra("name",name);
+                intent.putExtra("reg",reg);
+                intent.putExtra("collegename",collegename);
+                intent.putExtra("collegecity",collegecity);
+                intent.putExtra("address",address);
+                intent.putExtra("bloodgrp",bloodgrp);
+                intent.putExtra("mob",mob);
+                intent.putExtra("branch",branch);
+
+                startActivity(intent);
+            }
+        });
+
     }
+    /*
     public void openActivity(View view){
         Toast.makeText(this, "Opening your ID card", Toast.LENGTH_SHORT).show();
         Intent intent=new Intent(this,printid.class);
@@ -40,5 +104,21 @@ public class MainActivity extends AppCompatActivity {
         intent_reg.putExtra(EXTRA_REG,regtext);
         //
         startActivity(intent_reg);
+    }
+
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (resultCode==RESULT_OK){
+            if (requestCode==CAMERA_REQ_CODE){
+                //for camera
+                Bitmap img=(Bitmap)(data.getExtras().get("data"));
+                dp.setImageBitmap(img);
+
+            }
+        }
     }
 }
--- printid.java
+++ printid.java
@@ -7,13 +7,17 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class printid extends AppCompatActivity {
+    /*
     TextView printname;
     TextView printreg;
+     */
+    TextView printname,printreg,print_collegename,print_collegecity,print_address,print_bloodgrp,print_mob,print_branch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_printid);
+        /*
         //printing name
         printname=findViewById(R.id.print_name);
         Intent intent=getIntent();
@@ -25,5 +29,40 @@ public class printid extends AppCompatActivity {
         Intent intent_reg=getIntent();
         String reg=intent_reg.getStringExtra(MainActivity.EXTRA_REG);
         printreg.setText("hi" + reg);
+         */
+
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        String reg=intent.getStringExtra("reg");
+        String collegename=intent.getStringExtra("collegename");
+        String collegecity=intent.getStringExtra("collegecity");
+        String address=intent.getStringExtra("address");
+        String bloodgrp=intent.getStringExtra("bloodgrp");
+        String mob=intent.getStringExtra("mob");
+        String branch=intent.getStringExtra("branch");
+
+
+
+        printname=findViewById(R.id.print_name);
+        printreg=findViewById(R.id.print_reg);
+        print_collegename=findViewById(R.id.print_collegename);
+        print_collegecity=findViewById(R.id.print_collegecity);
+        print_address=findViewById(R.id.print_address);
+        print_bloodgrp=findViewById(R.id.print_bloodgrp);
+        print_mob=findViewById(R.id.print_mob);
+        print_branch=findViewById(R.id.print_branch);
+
+
+
+        printname.setText("name: "+name);
+        printreg.setText("registration number: "+reg);
+        print_collegename.setText(collegename);
+        print_collegecity.setText(collegecity);
+        print_address.setText("address: "+address);
+        print_bloodgrp.setText("bloodgrp: "+bloodgrp);
+        print_mob.setText("mobile number: "+mob);
+        print_branch.setText("branch: "+branch);
+
+
     }
 }
